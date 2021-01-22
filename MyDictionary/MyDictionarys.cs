ab596101aa77ba6d772541e93d5343082b6951ad
beyda1022@hotmail.com <beyda1022@hotmail.com>
2021-01-23 01:00:02 +0300

    Add project files.

---
+++ MyDictionary/MyDictionarys.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionarys<T,V>
+    {
+        
+            T[] item;
+            T[] tempitem;
+            V[] value;
+            V[] tempvalue;
+            public MyDictionarys()
+            {
+                item = new T[0];
+                value = new V[0];
+            }
+            public void Add(T items, V values)
+            {
+                tempitem = item;
+                tempvalue = value;
+                item = new T[item.Length + 1];
+                value = new V[value.Length + 1];
+
+                for (int i = 0; i < tempitem.Length; i++)
+                {
+                    item[i] = tempitem[i];
+                    value[i] = tempvalue[i];
+                }
+
+                item[item.Length - 1] = items;
+                value[value.Length - 1] = values;
+            }
+            public int Length
+            {
+                get { return item.Length; }
+            }
+
+            public T[] Item
+            {
+                get { return item; }
+            }
+            public V[] Value
+            {
+                get { return value; }
+            }
+        
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionarys<string, int> kisiler = new MyDictionarys<string, int>();
+            kisiler.Add("zişan", 22);
+            kisiler.Add("müşoo", 23);
+            kisiler.Add("küpp", 22);
+            for (int i = 0; i < kisiler.Item.Length; i++)
+            {
+                Console.WriteLine( kisiler.Item[i] + " - " + kisiler.Value[i]);
+            }
+
+        }
+    }
+   
+ 
+
+}
